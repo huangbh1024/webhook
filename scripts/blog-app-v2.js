ff463e52609bd8bc80cf0d46fd7cc8a8240e99c1
huangbh <578092215@qq.com>
2024-01-31 16:37:06 +0800

    新增网站

---
+++ scripts/blog-app-v2.js
@@ -0,0 +1,6 @@
+const { urlMap } = require('../utils/urlMap.js')
+const { startUpload } = require('../utils/qiniu.js')
+
+const basePath = `${urlMap['blog-app-v2']}/.next/static`;
+
+startUpload('_next/static', basePath)
--- utils/urlMap.js
+++ utils/urlMap.js
@@ -1,6 +1,7 @@
 const urlMap = {
     'blog-nuxt-v2': '/home/blog-nuxt-v2',
     'blog-server-v2': '/home/blog-server-v2',
+    'blog-app-v2': '/home/blog-app-v2',
     webhook: '/home/webhook',
 }
 
